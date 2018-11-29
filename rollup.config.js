19d1d6b7ac71ec86886e59ea880506dad18dd437
Olivier Refalo <orefalo@yahoo.com>
2018-11-29 23:15:01 +0400

    remove babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from "rollup-plugin-babel";
+// import babel from "rollup-plugin-babel";
 import pegjs from "rollup-plugin-pegjs";
 
 export default {
@@ -7,5 +7,5 @@ export default {
     file: "dist/graphql-shorthand.js",
     format: "cjs"
   },
-  plugins: [pegjs(), babel()]
+  plugins: [pegjs()]
 };
